bcfa9f0ac558ede28f73e002c707079a4a3e5730
liujun <jliu@topxgun.com>
2017-10-01 11:07:51 +0800

    add Components submodule

---
+++ Components/components/pd-header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="pd-header flex">
+        <div class="fluid">{{comName}}</div>
+        <div class="fixed">666</div>
+        <div class="fluid">666</div>        
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            comName: 'padaio'
+        }
+    }
+}
+</script>
+<style scoped>
+.pd-header {
+  box-shadow: 1px 1px 1px #f2f2f2;
+  background: url(../assets/avatar.png)
+}
+
+.fluid {
+  flex: 1;
+}
+
+.fixed {
+  width: 100%;
+  max-width: 960px;
+}
+</style>
